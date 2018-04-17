9fbf0e15565e8a66419a89be1a62062742cde14d
Daniel Quinn <code@danielquinn.org>
2018-04-17 17:31:13 +0100

    Add explanations to the placeholders

--- aletheia/file_types/video/mp4.py
+++ aletheia/file_types/video/mp4.py
@@ -2,4 +2,8 @@ from ..base import LargeFile
 
 
 class Mp4File(LargeFile):
+    """
+    This is a placeholder.  If you feel like helping out, this would be an
+    awesome place to start :-)
+    """
     pass
--- aletheia/file_types/video/webm.py
+++ aletheia/file_types/video/webm.py
@@ -2,4 +2,8 @@ from ..base import LargeFile
 
 
 class WebmFile(LargeFile):
+    """
+    This is a placeholder.  If you feel like helping out, this would be an
+    awesome place to start :-)
+    """
     pass
