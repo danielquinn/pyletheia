89e6d64c700d6c264ed0a2570d23f3cd6a07a48a
Daniel Quinn <code@danielquinn.org>
2018-04-29 22:38:11 +0100

    Add new exceptions for various edge cases

--- aletheia/exceptions.py
+++ aletheia/exceptions.py
@@ -1,2 +1,14 @@
 class UnknownFileTypeError(Exception):
     pass
+
+
+class InvalidURLError(Exception):
+    pass
+
+
+class PublicKeyNotExistsError(Exception):
+    pass
+
+
+class UnparseableFileError(Exception):
+    pass
